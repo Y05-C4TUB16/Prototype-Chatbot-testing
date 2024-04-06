b92e92762ee76447c9471334706149bb8d35e516
keithpascual <pascual.249469@ortigas-cainta.sti.edu.ph>
2024-04-07 02:46:45 +0800

    inquiry trial
    julius eto may tinry ako, itrain mo muna and irun actions bago itry yung chatbot...again refer ka lang sa section 4 kung pano nya ginawa, dun lang din ako nagbase haha i cant train it kasi kulang yung files, nasayo yung mga python and stuff. thanks pre
    
    train code: rasa train
    run action code: rasa run actions
    run bot: rasa shell

--- rasa_chatbot_test/actions/actions.py
+++ rasa_chatbot_test/actions/actions.py
@@ -1,27 +1,29 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
 
-# This is a simple example for a custom action which utters "Hello World!"
+class ActionAskScholarship(Action):
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+    def name(self) -> Text:
+        return "action_ask_scholarship"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # scholarship = tracker.latest_message['text']
+        # ask_scholarship = self.ask_renew_scholarship(scholarship)
+
+        scholarship_slot = next(tracker.get_latest_entity_values("scholarship"), None)
+        scholarship = tracker.get_slot("scholarship") or scholarship_slot
+
+        if scholarship:
+            response = "You may renew your scholarship in the registrars office upon enrollment."
+        else:
+            response = "Sorry I could not provide any info regarding your scholarship renewal."
+
+        dispatcher.utter_message(text=response)
+
+        return []
