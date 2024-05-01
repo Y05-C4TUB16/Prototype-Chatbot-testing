e8e3d4638925a42093e5cfc1020293f52aa879e1
keithpascual <152584026+keithpascual@users.noreply.github.com>
2024-05-01 21:22:44 +0800

    added fallback
    added default fallbacks, minor adjustments, and comments as reminders to me (what i will add later on)

--- rasa_chatbot_test/actions/actions.py
+++ rasa_chatbot_test/actions/actions.py
@@ -2,31 +2,32 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import UserUtteranceReverted
 
 
-class ActionAskScholarship(Action):
+# class ActionAskScholarship(Action):
 
-    def name(self) -> Text:
-        return "action_ask_scholarship"
+#     def name(self) -> Text:
+#         return "action_ask_scholarship"
 
-    def run(self, dispatcher: CollectingDispatcher,
-            tracker: Tracker,
-            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # scholarship = tracker.latest_message['text']
-        # ask_scholarship = self.ask_renew_scholarship(scholarship)
+#         # scholarship = tracker.latest_message['text']
+#         # ask_scholarship = self.ask_renew_scholarship(scholarship)
 
-        scholarship_slot = next(tracker.get_latest_entity_values("scholarship"), None)
-        scholarship = tracker.get_slot("scholarship") or scholarship_slot
+#         scholarship_slot = next(tracker.get_latest_entity_values("scholarship"), None)
+#         scholarship = tracker.get_slot("scholarship") or scholarship_slot
 
-        if scholarship:
-            response = "You may renew your scholarship in the registrars office upon enrollment."
-        else:
-            response = "Sorry I could not provide any info regarding your scholarship renewal."
+#         if scholarship:
+#             response = "You may renew your scholarship in the registrars office upon enrollment."
+#         else:
+#             response = "Sorry I could not provide any info regarding your scholarship renewal."
 
-        dispatcher.utter_message(text=response)
+#         dispatcher.utter_message(text=response)
 
-        return []
+#         return []
 
 
 class ActionAskStrand(Action):
@@ -45,8 +46,7 @@ class ActionAskStrand(Action):
         strand = tracker.get_slot("strand") or strand_slot
 
         if strand:
-            response = ("If you wish to know more about our available programs you may visit the school "
-                        "for more a knowledge")
+            response = ("If you wish to know more about our available programs you may visit the school for more a knowledge")
         else:
             response = "Sorry, I could not provide any info regarding your question."
 
@@ -91,3 +91,59 @@ class ActionProvideProgramInformation(Action):
 
         return []
 
+
+class ActionDefaultFallback(Action):
+
+    def name(self) -> Text:
+        return "action_default_fallback"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(response="utter_default")
+
+        return [UserUtteranceReverted()]
+
+# class ActionTwoStageFallback(Action):
+# 
+#     def name(self) -> Text:
+#         return "action_two_stage_fallback"
+# 
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+# 
+#         dispatcher.utter_message(response="utter_default")
+# 
+#         return [UserUtteranceReverted()]
+
+
+# This can be used when the user is given options:
+# class ActionArrayString(Action): 
+#     def name(self) -> Text:
+#         return "action_array_string"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#       'entity' = next(tracker.get_latest_entity_values("'entity'"), None)
+#
+#       if 'entity' = "'string'":
+#           'intent' = ["sample", "sample", "sample"]
+#       elif 'entity' = "'string'":
+#           'intent' = ["sample", "sample", "sample"]
+#       elif 'entity' = "'string'":
+#           'intent' = ["sample", "sample", "sample"]
+#       else:
+#           'intent' = []
+#
+#       if 'intent':
+#           response = f"blablabla  {'entity'} blablablabla" + "." .join('intent')
+#       else:
+#           response = "sorry boi"
+#
+#       dispatcher.utter_message(text=response)
+#
+#       return []
